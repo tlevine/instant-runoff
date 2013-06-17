93729578c03f0d5812baa32ada455fb7deffa09c
Thomas Levine <occurrence@thomaslevine.com>
2013-06-17 12:49:57 -0700

    annotate tests

--- test.py
+++ test.py
@@ -5,18 +5,21 @@ import nose.tools as n
 import instant_runoff as ir
 
 def test_txt():
+    'The text email should be generated as expected.'
     expected = open('fixtures/README.txt').read()
     input = open('fixtures/README.groff').read()
     observed = ir.groff_txt(input)
     n.assert_list_equal(observed.split('\n'), expected.split('\n'))
 
 def test_html():
+    'The HTML email should be generated as expected.'
     expected = open('fixtures/README.html').read()
     input = open('fixtures/README.groff').read()
     observed = ir.groff_html(input)
     n.assert_list_equal(observed.split('\n'), expected.split('\n'))
 
 def test_eml():
+    'The resulting multipart email should be generated as expected.'
     expected = open('fixtures/README.eml').read()
     input = open('fixtures/README.groff').read()
 
