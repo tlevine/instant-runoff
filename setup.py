62725247242d0cb25c88ed577ce221ba5f501f8f
Thomas Levine <_@thomaslevine.com>
2014-06-21 17:11:26 -0400

    python2

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(name='instant-runoff',
+      author='Robert Ochshorn',
+      author_email='mail@rmozone.com',
+      maintainer='Thomas Levine',
+      maintainer_email='_@thomaslevine.com',
+      description='Write emails in groff.',
+      url='http://readmes.numm.org/instant-runoff/',
+      scripts = ['instant-runoff'],
+      install_requires = [],
+      version='0.1',
+      license='AGPL',
+)
